5285201acaf252d6ccc762759ef13f848725e0c4
Miguel <miguelservel@gmail.com>
2025-07-23 16:23:57 +0100

    Updated FANTOM download repositories

--- VarGen.R
+++ VarGen.R
@@ -48,14 +48,23 @@ vargen_install <- function(install_dir = "./", gtex_version = "v10", timeout = 1
   # Download FANTOM5 Enhancers
   #-----------------------------------------------------------------------------
   if(verbose) print("Dowloading FANTOM's enhancer tss associations")
-  # FANTOM5 TSS associations old link:
-  #utils::download.file("http://enhancer.binf.ku.dk/presets/enhancer_tss_associations.bed",
-  #                     destfile = paste0(install_dir, "/enhancer_tss_associations.bed"))
 
   # FANTOM5 new link:
-  utils::download.file("https://slidebase.binf.ku.dk/human_enhancers/presets/serve/enhancer_tss_associations",
-                       destfile = paste0(install_dir, "/enhancer_tss_associations.bed"))
-
+  # utils::download.file("https://slidebase.binf.ku.dk/human_enhancers/presets/serve/enhancer_tss_associations",
+  #                      destfile = paste0(install_dir, "/enhancer_tss_associations.bed"))
+  
+  # FANTOM5 enhancers
+  utils::download.file("https://fantom.gsc.riken.jp/5/datafiles/reprocessed/hg38_latest/extra/enhancer/F5.hg38.enhancers.bed.gz",
+                       destfile = paste0(install_dir, "/F5.hg38.enhancers.bed.gz"))
+  R.utils::gunzip(filename = paste0(install_dir, "/F5.hg38.enhancers.bed.gz"),
+                  skip = TRUE, remove = TRUE)
+  
+  # FANTOM5 promoters
+  utils::download.file("https://fantom.gsc.riken.jp/5/datafiles/reprocessed/hg38_latest/extra/CAGE_peaks_expression/hg38_liftover+new_CAGE_peaks_phase1and2_ann.txt.gz",
+                       destfile = paste0(install_dir, "/hg38_liftover+new_CAGE_peaks_phase1and2_ann.txt.gz"))
+  R.utils::gunzip(filename = paste0(install_dir, "/hg38_liftover+new_CAGE_peaks_phase1and2_ann.txt.gz"),
+                  skip = TRUE, remove = TRUE)
+  
   cat("\n")
 
   
